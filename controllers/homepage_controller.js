fcfce190b5838e79cdafef9a4fcefc9294a40ab2
Eric Liu <eric.liu85@gmail.com>
2018-06-23 11:11:56 -0700

    Update homepage_controller.js

--- controllers/homepage_controller.js
+++ controllers/homepage_controller.js
@@ -2,7 +2,10 @@ var db = require('../models');
 
 exports.index = function (req, res) {
   db.Video_Stream.findAll({
-    include: [db.Screenshot]
+    include: [db.Screenshot],
+    where: {
+      UserId: req.user.id
+    }
   }).then(function (dbStream) {
     console.log(dbStream);
     res.render('homepage/homepage', {
