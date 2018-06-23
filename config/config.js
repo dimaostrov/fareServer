9f5c72d4572a10b1b9d86d56791ee84db8c91ee9
Dmitriy Ostrovskiy <dmtrostrovskiy@gmail.com>
2018-06-23 01:06:44 -0700

    Update config.js
    preserve the env variables and update production db link

--- config/config.js
+++ config/config.js
@@ -1,18 +1,11 @@
 module.exports = {
-  // "development": {
-  //   "username": process.env.DB_USER,
-  //   "password": process.env.DB_PASS,
-  //   "database": "face_recognitionDB",
-  //   "host": "127.0.0.1",
-  //   "dialect": "mysql"
-  // },
   "development": {
-    "username": "root",
-    "password": "rafa",
-    "database": "database_test",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
-  },
+     "username": process.env.DB_USER,
+     "password": process.env.DB_PASS,
+     "database": "face_recognitionDB",
+     "host": "127.0.0.1",
+     "dialect": "mysql"
+   },
   "test": {
     "username": "root",
     "password": "rafa",
@@ -21,7 +14,7 @@ module.exports = {
     "dialect": "mysql"
   },
   "production": {
-    "use_env_variable": "JAWSDB_URL",
+    "use_env_variable": process.env.JAWSDB_URL,
     "dialect": "mysql"
   }
 }
