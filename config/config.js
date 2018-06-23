48014489c76b4201e7ea349b66b944951618ce36
n0rthf4ce <eric.liu85@gmail.com>
2018-06-23 03:58:02 -0700

    add delete button and database loading. need to fix associations with screenshots and improve ux/ui

--- config/config.js
+++ config/config.js
@@ -1,17 +1,10 @@
 module.exports = {
-  // "development": {
-  //   "username": process.env.DB_USER,
-  //   "password": process.env.DB_PASS,
-  //   "database": "face_recognitionDB",
-  //   "host": "127.0.0.1",
-  //   "dialect": "mysql"
-  // },
-  "development": {
-    "username": "root",
-    "password": "rafa",
-    "database": "database_test",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+   "development": {
+     "username": process.env.DB_USER,
+     "password": process.env.DB_PASS,
+     "database": "face_recognitionDB",
+     "host": "127.0.0.1",
+     "dialect": "mysql"
   },
   "test": {
     "username": "root",
--- controllers/homepage_controller.js
+++ controllers/homepage_controller.js
@@ -2,20 +2,26 @@ var db = require('../models');
 
 exports.index = function (req, res) {
   db.Video_Stream.findAll({
-    include: [db.Screenshot],
-    where: {
-      UserId: req.user.id
-    }
+    include: [db.Screenshot]
   }).then(function (dbStream) {
     console.log(dbStream);
     res.render('homepage/homepage', {
       layout: 'main-streams',
       stream: dbStream,
       helpers: {
-        foo: function () { return 'foo.'; }
+        filterScreenshots: function (screenshots,title) {
+          var filtered=[];
+          for (let i = 0; i < screenshots.length; i++) {
+            const element = screenshots[i];
+            if(element.streamTitle==title){
+              filtered.push(element);
+            }
+          }
+          return filtered;
+        }
       }
     });
-  }); 
+  });
 };
 
 exports.createStream = function (req, res) {
@@ -24,8 +30,19 @@ exports.createStream = function (req, res) {
   // Add id from User onto req.body
   req.body.UserId = req.user.id;
 
-  db.Video_Stream.create(req.body).then(function (dbStream) {
+  db.Video_Stream.create(req.body, {
+    include: [db.Screenshot]
+  }).then(function (dbStream) {
     res.json(dbStream);
   });
 };
 
+exports.deleteStream = function(req, res) {
+  db.Video_Stream.destroy({
+    where: {
+      id: req.params.id
+    }
+  }).then(function(dbPost) {
+    res.json(dbPost);
+  });
+}
