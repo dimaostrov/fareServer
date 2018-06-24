d9a379cfa30247f2b0fc06ed287c3343edcd2a0c
Rafael <36059806+rafraga@users.noreply.github.com>
2018-06-24 00:31:38 -0700

    Update facedetection_js.js

--- public/facedect/facedetection_js.js
+++ public/facedect/facedetection_js.js
@@ -106,7 +106,7 @@ async function onPlay(videoEl) {
 
       new Promise(function (fulfill, reject) {
         document.getElementById("results-view").style.display = "none"
-        $("#faces_view").attr("style", "height:600px;width:100%;background-color:white;overflow-y:scroll;overflow-x:hidden;right: 0;left: 0;margin-right: auto;margin-left: auto;text-align: center")
+        $("#faces_view").attr("style", "height:400px;width:100%;background-color:white;overflow-y:scroll;overflow-x:hidden;right: 0;left: 0;margin-right: auto;margin-left: auto;text-align: center")
         $("#faces_view").html(faces)
         fulfill(result);
       }).then(function (result) {
