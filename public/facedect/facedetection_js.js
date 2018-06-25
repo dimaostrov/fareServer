fef75d1990bab330c80f5c2eacf4abf1c006b629
Rafael <36059806+rafraga@users.noreply.github.com>
2018-06-24 21:44:13 -0700

    Update facedetection_js.js

--- public/facedect/facedetection_js.js
+++ public/facedect/facedetection_js.js
@@ -92,6 +92,7 @@ async function onPlay(videoEl) {
   vid.onplaying = function () {
     vid.pause()
     vid.remove()
+    $('body').attr("style","overflow: auto")
     $('#results-view').scrollTop($('#results-view')[0].scrollHeight)
     $('#spaces').html("")
     end_time = current_time_sec
