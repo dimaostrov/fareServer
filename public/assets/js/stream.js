b1216d91de6a0ccbb5a84d54054fe1ceb7cdd87a
Rafael <36059806+rafraga@users.noreply.github.com>
2018-06-25 11:19:29 -0700

    Update stream.js

--- public/assets/js/stream.js
+++ public/assets/js/stream.js
@@ -1,3 +1,5 @@
+alert("This is a beta version, we are currently in a temporary test server that often goes down. If the loading takes too long, please check if https://cors-anywhere.herokuapp.com servers are working in your browser. We apologize for any inconvenience.")
+
 var nameInput = $("#streamName"),
 	urlInput = $("#streamUrl"),
 	screenshots = {};
