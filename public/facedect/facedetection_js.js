5b336a0b60f4418bc6601297f17442d21f6f8b39
Rafael <36059806+rafraga@users.noreply.github.com>
2018-06-25 09:15:58 -0700

    Update facedetection_js.js

--- public/facedect/facedetection_js.js
+++ public/facedect/facedetection_js.js
@@ -97,7 +97,7 @@ async function onPlay(videoEl) {
     $('#spaces').html("")
     end_time = current_time_sec
     $('#header').html("<center><h5>Results</h5><h6>" + count_faces + " total faces found in video segment (from " + start_time + " to " + end_time + " seconds).<br>Total images processed:" + total_frames_processed + "</h6><button class='btn' id='add-stream-button' data-toggle='modal' data-target='#squarespaceModal' type='button' style='position: center;right: 0;left: 0;margin-right: auto;margin-left: auto' value='Save Stream'><h6>Save Stream</h6></button>&nbsp;&nbsp;<button class='btn' id='download_image_button' style='position: center;right: 0;left: 0;margin-right: auto;margin-left: auto' value='Download Faces'><h6>Download Faces</h6></button></center>")
-    $('#header').attr("style", "height:20px;right: 0;left: 0;margin-right: auto;margin-left: auto;min-height: 7em;width: 100%;top: 30em;background-color:white")
+    $('#header').attr("style", "height:20px;right: 0;left: 0;margin-right: auto;margin-left: auto;min-height: 5em;width: 100%;top: 30em;background-color:white")
     console.log(results_data) // results that will go to the database
     console.log("total_faces_in_video: " + count_faces) // results that will go to the database
     $('#spaces').html("<br>")
