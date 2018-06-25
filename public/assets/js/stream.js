3d03e151d4d0bbac2f988812615d6d7221c5fa6c
Rafael <36059806+rafraga@users.noreply.github.com>
2018-06-25 11:35:32 -0700

    Update stream.js

--- public/assets/js/stream.js
+++ public/assets/js/stream.js
@@ -1,4 +1,4 @@
-alert("This is a beta version, we are currently in a temporary test server that often goes down. If the loading takes too long, please check if https://cors-anywhere.herokuapp.com servers are working in your browser. We apologize for any inconvenience.")
+alert("This is a beta version, we are currently in a temporary test server that often goes down. If the loading takes too long, please check if https://cors-anywhere.herokuapp.com servers are working in your browser. We apologize for any inconvenience. Google Chrome browser is required.")
 
 var nameInput = $("#streamName"),
 	urlInput = $("#streamUrl"),
