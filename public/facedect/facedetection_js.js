b5a04667c15bf608590a3032664e7dc9fdb4122e
Rafael <36059806+rafraga@users.noreply.github.com>
2018-06-25 08:57:16 -0700

    Update facedetection_js.js

--- public/facedect/facedetection_js.js
+++ public/facedect/facedetection_js.js
@@ -101,7 +101,7 @@ async function onPlay(videoEl) {
     console.log("total_faces_in_video: " + count_faces) // results that will go to the database
     $('#spaces').html("<br>")
     $('#spaces2').html("<br><br><br>")
-    $("#results-view").attr("style", "height:50px;width:700px;background-color:#eee;overflow-y:scroll;overflow-x:hidden;right: 0;left: 0;margin-right: auto;margin-left: auto;min-height: 25em;width: 90%;text-align: left")
+    $("#results-view").attr("style", "height:30px;width:700px;background-color:#eee;overflow-y:scroll;overflow-x:hidden;right: 0;left: 0;margin-right: auto;margin-left: auto;min-height: 25em;width: 90%;text-align: left")
 
     $('#download_image_button').click(function () {
 
